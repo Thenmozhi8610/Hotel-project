4ffb1bbb75d3e0ef9fc86f52a6c766bbd4570e2a
Thenmozhi8610 <thenmozhi8610@gmail.com>
2025-03-31 12:46:21 +0530

    new message

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -6,7 +6,9 @@ function Banner(){
         <div className="absolute w-screen left-0 ">
             <img src="banner.png" alt="Banner image"/>
             <div className="absolute top-45 left-200 text-white font-bold text-l size-30" >my kitchen</div>
+            <h1>hello woprld</h1>
         </div>
+       
     )
 }
 export default Banner
